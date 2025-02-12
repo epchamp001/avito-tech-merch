d90ce24bf4df145163f9cc6ef2e9f87958d816ad
Egor Ponyaev <epchamp001@gmail.com>
2025-02-12 20:40:25 +0300

    update

--- internal/models/user.go
+++ internal/models/user.go
@@ -8,7 +8,7 @@ import (
 type User struct {
 	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
 	Username  string    `json:"username" gorm:"uniqueIndex;not null"`
-	Password  string    `json:"password" gorm:"not null"` // В JSON лучше не возвращать пароль, поэтому можно делать json:"-"
+	Password  string    `json:"-" gorm:"not null"`
 	Balance   int       `json:"balance"  gorm:"not null;default:1000"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
